0c98e70077994c0e8802a358e64b28dbdd1ca4a3
Ifeanyi Osuji <oindubuisi@gmail.com>
2024-01-12 18:06:55 +0100

    Added class diagram

--- car_factory.py
+++ car_factory.py
@@ -9,23 +9,26 @@ from nubbin_battery import NubbinBattery
 
 
 class CarFactory:
-    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage)->Car:
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage)->Car:
         calliope = Car(engine=CapuletEngine(last_service_date, current_mileage, last_service_mileage), battery=SplinderBattery(last_service_date, current_date))
         return calliope
    
-    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage)->Car:
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage)->Car:
         glissade = Car(engine=WilloughbyEngine(last_service_date, current_mileage, last_service_mileage), battery=SplinderBattery(last_service_date, current_date))
         return glissade
-    
-    def create_palindrome(self, current_date, last_service_date, warning_light_is_on)->Car:
+    @staticmethod
+    def create_palindrome(current_date, last_service_date, warning_light_is_on)->Car:
         palindrome = Car(engine=SternmanEngine(last_service_date, warning_light_is_on), battery=SplinderBattery(last_service_date, current_date))
         return palindrome
     
-    def create_rorscharch(self, current_date, last_service_date, current_mileage, last_service_mileage)->Car:
+    @staticmethod
+    def create_rorscharch(current_date, last_service_date, current_mileage, last_service_mileage)->Car:
         rorscharch = Car(engine=WilloughbyEngine(last_service_date, current_mileage, last_service_mileage), battery=NubbinBattery(last_service_date, current_date))
         return rorscharch
-    
-    def create_thovax(self, current_date, last_service_date, current_mileage, last_service_mileage)->Car:
+    @staticmethod
+    def create_thovax(current_date, last_service_date, current_mileage, last_service_mileage)->Car:
         thovax = Car(engine=CapuletEngine(last_service_date, current_mileage, last_service_mileage), battery=NubbinBattery(last_service_date, current_date))
         return thovax
 
