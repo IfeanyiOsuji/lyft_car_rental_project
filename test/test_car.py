afde8924b149d8928fb4838dcef64603f7787094
Ifeanyi Osuji <oindubuisi@gmail.com>
2024-01-13 01:16:48 +0100

    debugged glissade car test

--- test/test_car.py
+++ test/test_car.py
@@ -50,42 +50,42 @@ class TestCalliope(unittest.TestCase):
         self.assertFalse(car.needs_service())
 
 
-# class TestGlissade(unittest.TestCase):
-#     def test_battery_should_be_serviced(self):
-#         today = datetime.today()
-#         last_service_date = today.date().replace(year=today.year - 3)
-#         current_mileage = 0
-#         last_service_mileage = 0
+class TestGlissade(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2016-01-25")
+        current_mileage = 0
+        last_service_mileage = 0
 
-#         car = CarFactory.create_glissade(today,last_service_date, current_mileage, last_service_mileage)
-#         self.assertTrue(car.needs_service())
+        car = CarFactory.create_glissade(last_service_date,current_date, current_mileage, last_service_mileage)
+        self.assertTrue(car.needs_service())
 
-#     def test_battery_should_not_be_serviced(self):
-#         today = datetime.today()
-#         last_service_date = today.date().replace(year=today.year - 1)
-#         current_mileage = 0
-#         last_service_mileage = 0
+    def test_battery_should_not_be_serviced(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2019-01-25")
+        current_mileage = 0
+        last_service_mileage = 0
 
-#         car = CarFactory.create_glissade(today, last_service_date, current_mileage, last_service_mileage)
-#         self.assertFalse(car.needs_service())
+        car = CarFactory.create_glissade(last_service_date,current_date, current_mileage, last_service_mileage)
+        self.assertFalse(car.needs_service())
 
-#     def test_engine_should_be_serviced(self):
-#         today = datetime.today().date()
-#         last_service_date = datetime.today().date()
-#         current_mileage = 60001
-#         last_service_mileage = 0
+    def test_engine_should_be_serviced(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2020-05-25")
+        current_mileage = 60001
+        last_service_mileage = 0
 
-#         car = CarFactory.create_glissade(today,last_service_date, current_mileage, last_service_mileage)
-#         self.assertTrue(car.needs_service())
+        car = CarFactory.create_glissade(last_service_date,current_date, current_mileage, last_service_mileage)
+        self.assertTrue(car.needs_service())
 
-#     def test_engine_should_not_be_serviced(self):
-#         today = datetime.today()
-#         last_service_date = datetime.today().date()
-#         current_mileage = 60000
-#         last_service_mileage = 0
+    def test_engine_should_not_be_serviced(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2020-01-25")
+        current_mileage = 60000
+        last_service_mileage = 0
 
-#         car = CarFactory.create_glissade(today, last_service_date, current_mileage, last_service_mileage)
-#         self.assertFalse(car.needs_service())
+        car = CarFactory.create_glissade(last_service_date,current_date, current_mileage, last_service_mileage)
+        self.assertFalse(car.needs_service())
 
 
 # class TestPalindrome(unittest.TestCase):
