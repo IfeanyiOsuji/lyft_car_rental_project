8c18b5483c3254eabcb4653560eed264ea7d5324
Ifeanyi Osuji <oindubuisi@gmail.com>
2024-01-13 01:30:35 +0100

    debugged thovax car test

--- test/test_car.py
+++ test/test_car.py
@@ -160,42 +160,42 @@ class TestRorschach(unittest.TestCase):
         self.assertFalse(car.needs_service())
 
 
-# class TestThovex(unittest.TestCase):
-#     def test_battery_should_be_serviced(self):
-#         today = datetime.today()
-#         last_service_date = today.date().replace(year=today.year - 5)
-#         current_mileage = 0
-#         last_service_mileage = 0
+class TestThovex(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2015-01-25")
+        current_mileage = 0
+        last_service_mileage = 0
 
-#         car = CarFactory.create_thovax(today, last_service_date, current_mileage, last_service_mileage)
-#         self.assertTrue(car.needs_service())
+        car = CarFactory.create_thovax(last_service_date,current_date, current_mileage, last_service_mileage)
+        self.assertTrue(car.needs_service())
 
-#     def test_battery_should_not_be_serviced(self):
-#         today = datetime.today()
-#         last_service_date = today.date().replace(year=today.year - 3)
-#         current_mileage = 0
-#         last_service_mileage = 0
+    def test_battery_should_not_be_serviced(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2017-01-25")
+        current_mileage = 0
+        last_service_mileage = 0
 
-#         car = CarFactory.create_thovax(today, last_service_date, current_mileage, last_service_mileage)
-#         self.assertFalse(car.needs_service())
+        car = CarFactory.create_thovax(last_service_date,current_date, current_mileage, last_service_mileage)
+        self.assertFalse(car.needs_service())
 
-#     def test_engine_should_be_serviced(self):
-#         today = datetime.today()
-#         last_service_date = datetime.today().date()
-#         current_mileage = 30001
-#         last_service_mileage = 0
+    def test_engine_should_be_serviced(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2020-01-25")
+        current_mileage = 30001
+        last_service_mileage = 0
 
-#         car = CarFactory.create_thovax(today, last_service_date, current_mileage, last_service_mileage)
-#         self.assertTrue(car.needs_service())
+        car = CarFactory.create_thovax(last_service_date,current_date, current_mileage, last_service_mileage)
+        self.assertTrue(car.needs_service())
 
-#     def test_engine_should_not_be_serviced(self):
-#         today = datetime.today()
-#         last_service_date = datetime.today().date()
-#         current_mileage = 30000
-#         last_service_mileage = 0
+    def test_engine_should_not_be_serviced(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2020-01-25")
+        current_mileage = 30000
+        last_service_mileage = 0
 
-#         car = CarFactory.create_thovax(today, last_service_date, current_mileage, last_service_mileage)
-#         self.assertFalse(car.needs_service())
+        car = CarFactory.create_thovax(last_service_date,last_service_date, current_mileage, last_service_mileage)
+        self.assertFalse(car.needs_service())
 
 
 if __name__ == '__main__':
